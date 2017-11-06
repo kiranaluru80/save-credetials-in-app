212da38a6c97d76c86dd55ff4423651c3198156e
kiranaluru80 <31380802+kiranaluru80@users.noreply.github.com>
2017-11-06 08:03:02 -0500

    save userid

---
+++ SamplePage/SamplePage/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SamplePage
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+         
+            MainPage = new NavigationPage(new FirstPage());
+        }
+    }
+}
---
+++ SamplePage/SamplePage/FirstPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SamplePage
+{
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+
+            //Save user name to App Current properties
+            App.Current.Properties["LoginUserName"] = "Ravi";
+            App.Current.SavePropertiesAsync();
+            //End
+
+            //Get username from Save properties
+            if (App.Current.Properties.ContainsKey("LoginUserName")){
+                string userName = App.Current.Properties["LoginUserName"].ToString();
+            }
+			//end
+
+
+			//Convert string to base 64 encoding
+			byte[] bytes = Encoding.UTF8.GetBytes("Ravi");
+			string base64 = Convert.ToBase64String(bytes);
+			Console.WriteLine(base64);
+            //end
+
+            //Decode base64 string to normal string
+            byte[] bytesData = Convert.FromBase64String(base64);
+			string str = Encoding.UTF8.GetString(bytesData);
+			Console.WriteLine(str);
+            //end
+
+        }
+    }
+}
